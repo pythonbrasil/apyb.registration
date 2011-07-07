5a19512790966fec22ad22d6bb8ddfeccd491566
Érico Andrei <ericof@gmail.com>
2011-07-07 19:16:20 -0300

    Initial import

---
+++ apyb/registration/tests.py
@@ -0,0 +1,62 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import apyb.registration
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['apyb.registration'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              apyb.registration)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='apyb.registration',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='apyb.registration.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt', 
+            package='apyb.registration',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'BROWSER.TXT',
+        #    package='apyb.registration',
+        #    optionflags = OPTION_FLAGS,
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5'
+
+setup(name='apyb.registration',
+      version=version,
+      description="Conference registration package",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['apyb'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
