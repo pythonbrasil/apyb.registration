b1103e705a039a37b711288e1d7d97c63ecc4096
Érico Andrei <erico@simplesconsultoria.com.br>
2011-07-12 01:37:30 -0300

    Add pricing information

---
+++ apyb/registration/config.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+PRICES = {
+          'apyb':13500,
+          'student':15000,
+          'individual':22500,
+          'government':40000,
+          'group':30000,
+         }
+
+GROUP_DISCOUNTS = {
+    'group':[
+        (3,0.25),
+        (5,0.27),
+        (7,0.32),
+        (10,0.42),
+        ]
+}
