63faa65a4500b1b57050a9b2e31bfd9d5caf20e0
Érico Andrei <erico@simplesconsultoria.com.br>
2011-08-04 22:17:08 -0300

    Add fmtPrice function

--- apyb/registration/utils.py
+++ apyb/registration/utils.py
@@ -3,6 +3,15 @@ from random import choice
 VALIDCHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
 SIZE = 8
 
+def fmtPrice(price):
+    ''' Format a value as price'''
+    price = str(price).strip()
+    if price:
+        if price == '0':
+            price = '0000'
+        return 'R$ %s,%s' % (price[:-2],price[-2:])
+    else:
+        return '-'
 def generateId():
     ''' Generate an id '''
     uid = [choice(VALIDCHARS) for i in range(0,SIZE+1)] 
