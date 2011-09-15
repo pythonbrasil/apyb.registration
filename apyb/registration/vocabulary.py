f421584435a77d7b5c1dd5a3c2b603eb37ad85d7
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-14 22:39:16 -0300

    Add apyb.registration.trainings vocabulary and PEP8 compliancy

--- apyb/registration/vocabulary.py
+++ apyb/registration/vocabulary.py
@@ -10,67 +10,85 @@ from Products.CMFCore.utils import getToolByName
 from apyb.registration import MessageFactory as _
 
 # registerSimpleVocabulary calls require the module global dictionary
-# as the last parameter in order for grok to register its variables 
-# in this module (normally it would be done silently 
+# as the last parameter in order for grok to register its variables
+# in this module (normally it would be done silently
 # and implicitly by a call to grok.global_utils
 
 registerSimpleVocabulary(
-    "Gender", u"apyb.registration", 
-    [('m',_(u'Male')),
-     ('f',_(u'Female')),
+    "Gender", u"apyb.registration",
+    [('m', _(u'Male')),
+     ('f', _(u'Female')),
     ],
-    globals()
+    globals(),
 )
 
 registerSimpleVocabulary(
-    "TShirt", u"apyb.registration", 
+    "TShirt", u"apyb.registration",
     [
-     ('S',_(u'Small')),
-     ('M',_(u'Medium')),
-     ('L',_(u'Large')),
-     ('X',_(u'X-Large')),
+     ('S', _(u'Small')),
+     ('M', _(u'Medium')),
+     ('L', _(u'Large')),
+     ('X', _(u'X-Large')),
     ],
-    globals()
+    globals(),
 )
 
 registerSimpleVocabulary(
     "Types", u"apyb.registration",
     [
-     ('apyb',_(u'APyB Members')),
-     ('student',_(u'Student')),
-     ('individual',_(u'Individual')),
-     ('government',_(u'Government')),
-     ('group',_(u'Group/Corporate')),
-     ('speaker',_(u'Speaker')),
-     ('sponsor',_(u'Sponsor')),
-     ('organizer',_(u'Organization')),
+     ('apyb', _(u'APyB Members')),
+     ('student', _(u'Student')),
+     ('individual', _(u'Individual')),
+     ('government', _(u'Government')),
+     ('group', _(u'Group/Corporate')),
+     ('speaker', _(u'Speaker')),
+     ('sponsor', _(u'Sponsor')),
+     ('organizer', _(u'Organization')),
     ],
-    globals()
+    globals(),
 )
 
 registerSimpleVocabulary(
     "PaymentServices", u"apyb.registration",
     [
-     ('paypal',_(u'PayPal')),
-     ('pagseguro',_(u'Pagseguro')),
+     ('paypal', _(u'PayPal')),
+     ('pagseguro', _(u'Pagseguro')),
     ],
-    globals()
+    globals(),
 )
+
 
 def attendeesVocabulary(context):
     """Vocabulary factory for attendees
     """
-    ct = getToolByName(context,'portal_catalog')
-    dictSearch = {'portal_type':'apyb.registration.attendee',
-                  'sort_on':'sortable_title',
-                  'review_state':'confirmed'}
+    ct = getToolByName(context, 'portal_catalog')
+    dictSearch = {'portal_type': 'apyb.registration.attendee',
+                  'sort_on': 'sortable_title',
+                  'review_state': 'confirmed'}
     attendees = ct.searchResults(**dictSearch)
-    attendees = [SimpleTerm(b.UID,b.UID,b.Title) for b in attendees]
+    attendees = [SimpleTerm(b.UID, b.UID, b.Title) for b in attendees]
     return SimpleVocabulary(attendees)
 
 registerSimpleVocabulary(
     "Attendees", u"apyb.registration",
     attendeesVocabulary,
-    globals()
+    globals(),
 )
 
+
+def trainingsVocabulary(context):
+    """Vocabulary factory for trainings
+    """
+    ct = getToolByName(context, 'portal_catalog')
+    dictSearch = {'portal_type': 'apyb.papers.training',
+                  'sort_on': 'sortable_title',
+                  'review_state': 'confirmed'}
+    trainings = ct.searchResults(**dictSearch)
+    trainings = [SimpleTerm(b.UID, b.UID, b.Title) for b in trainings]
+    return SimpleVocabulary(trainings)
+
+registerSimpleVocabulary(
+    "Trainings", u"apyb.registration",
+    trainingsVocabulary,
+    globals(),
+)
