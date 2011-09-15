a6d6d85e5e2c9bfd880e97fb7fd9c07ffa594a46
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-14 23:49:11 -0300

    Added available_trainings method PEP8

--- apyb/registration/registration.py
+++ apyb/registration/registration.py
@@ -8,9 +8,6 @@ from zope.component import getMultiAdapter, queryUtility
 
 from zope import schema
 
-from z3c.form import group, field
-from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
-
 from zope.component import getUtility
 from zope.app.intid.interfaces import IIntIds
 
@@ -19,6 +16,7 @@ from plone.indexer import indexer
 from apyb.registration.utils import generateId
 
 from apyb.registration import MessageFactory as _
+
 
 class IRegistration(form.Schema):
     """
@@ -29,20 +27,20 @@ class IRegistration(form.Schema):
         title=_(u"uid"),
         required=False,
         )
-    
+    #
     email = schema.TextLine(
            title=_(u'E-mail'),
            description=_(u'Please provide an email address'),
            required=True,
     )
-    
+    #
     registration_type = schema.Choice(
         title=_(u'Type'),
         description=_(u'Select the category of your registration'),
         required=True,
-        vocabulary="apyb.registration.types"
+        vocabulary="apyb.registration.types",
         )
-    
+    #
     discount_code = schema.TextLine(
         title=_(u'Discount code'),
         description=_(u'If you have a discount code, please inform here'),
@@ -54,46 +52,51 @@ class IRegistration(form.Schema):
 @indexer(IRegistration)
 def num_attendees(obj):
     children = obj.objectValues()
-    children = [c for c in children if c.portal_type=='apyb.registration.attendee']
+    children = [c for c in children
+                        if c.portal_type=='apyb.registration.attendee']
     return len(children)
 grok.global_adapter(num_attendees, name="num_attendees")
 
+
 @indexer(IRegistration)
 def price_est(obj):
-    if not getattr(obj,'paid',False):
+    if not getattr(obj, 'paid', False):
         children = obj.objectValues()
-        children = [c for c in children if c.portal_type=='apyb.registration.attendee']
-        
+        children = [c for c in children
+                            if c.portal_type=='apyb.registration.attendee']
+        #
         view = aq_parent(obj).restrictedTraverse('@@reg-price')
         qty = len(children)
-        
+        #
         registration_type = obj.registration_type
         discount_code = obj.discount_code
-        price = view.price(registration_type,qty,discount_code)
+        price = view.price(registration_type, qty, discount_code)
     else:
         price = obj.amount
     return price
 
 grok.global_adapter(price_est, name="price_est")
 
+
 @indexer(IRegistration)
 def registration_type(obj):
-    return [obj.registration_type,]
+    return [obj.registration_type, ]
 grok.global_adapter(registration_type, name="Subject")
+
 
 class Registration(dexterity.Container):
     grok.implements(IRegistration)
-    
+    #
     def __init__(self, id=None, **kwargs):
         if not id:
             id = generateId()
-        super(Registration,self).__init__(id=id, **kwargs)
-    
+        super(Registration, self).__init__(id=id, **kwargs)
+    #
     @property
     def uid(self):
         intids = getUtility(IIntIds)
         return intids.getId(self)
-    
+    #
     def UID(self):
         return self.uid
 
@@ -101,100 +104,111 @@ class Registration(dexterity.Container):
 class View(grok.View):
     grok.context(IRegistration)
     grok.require('zope2.View')
-    
+    #
     def update(self):
-        super(View,self).update()
+        super(View, self).update()
         context = aq_inner(self.context)
+        registrations = aq_parent(self.context)
         self.registration_type = context.registration_type
         self._path = '/'.join(context.getPhysicalPath())
-        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
-        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
-        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self.state = getMultiAdapter((context, self.request),
+                                      name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request),
+                                      name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request),
+                                       name=u'plone_portal_state')
+        self.helper = getMultiAdapter((registrations, self.request),
+                                      name=u'helper')
         self._ct = self.tools.catalog()
         self._mt = self.tools.membership()
         self.member = self.portal.member()
-        self.price_view = aq_parent(self.context).restrictedTraverse('@@reg-price')
-        self.voc = queryUtility(IVocabularyFactory, 'apyb.registration.types')(context)
-        roles_context = self.member.getRolesInContext(context)
-        if not [r for r in roles_context if r in ['Manager','Editor','Reviewer',]]:
+        self.price_view = registrations.restrictedTraverse('@@reg-price')
+        voc_factory = queryUtility(IVocabularyFactory,
+                                   'apyb.registration.types')
+        self.voc = voc_factory(context)
+        self.roles_context = self.member.getRolesInContext(context)
+        if not [r for r in self.roles_context
+                        if r in ['Manager', 'Editor', 'Reviewer', ]]:
             self.request['disable_border'] = True
-    
+    #
     def attendees(self):
         ct = self._ct
         path = self._path
-        results = ct.searchResults(portal_type='apyb.registration.attendee',path=path)
+        results = ct.searchResults(portal_type='apyb.registration.attendee',
+                                   path=path)
         return results
-    
+    #
     @property
     def creator(self):
         creator = self.context.Creator()
         data = []
-        if isinstance(creator,str):
-            creator = [creator,]
+        if isinstance(creator, str):
+            creator = [creator, ]
         for member_id in creator:
             member = self._mt.getMemberById(member_id)
             if member:
-                member = '%s <%s>' % (member.getProperty('fullname') or member_id,
-                               member.getProperty('email') or member_id)
+                member_fullname = member.getProperty('fullname') or member_id
+                member_email = member.getProperty('email') or member_id
+                member = '%s <%s>' % (member_fullname, member_email)
             else:
                 member = member_id
             data.append(member)
         return ', '.join(data)
-    
+    #
     @property
     def created(self):
         created = self.context.creation_date
         return created.strftime('%d/%m/%Y %H:%M')
-    
+    #
     @property
     def fmt_registration_type(self):
         registration_type = self.registration_type
-        return self.voc.getTerm(registration_type).title
-    
+        term = self.voc.getTerm(registration_type)
+        return term.title
+    #
     @property
     def show_payments(self):
         state = self.state.workflow_state()
-        return not (self.paid  or state == 'confirmed')
-    
+        return not (self.paid or state == 'confirmed')
+    #
     @property
     def paid(self):
-        is_paid = getattr(self.context,'paid',False)
-        if self.registration_type in ['organizer','sponsor']:
+        is_paid = getattr(self.context, 'paid', False)
+        if self.registration_type in ['organizer', 'sponsor']:
             is_paid = True
         return is_paid
-    
+    #
     @property
     def show_empenho(self):
         ''' show only if registration type == government'''
         return self.registration_type == 'government'
-    
+    #
     @property
     def show_pagseguro(self):
-        ''' show only if registration not paid and 
+        ''' show only if registration not paid and
             registration is from brazil
         '''
         country = self.context.country
         paid = self.paid
-        return (country==u'br' and not (paid or self.show_empenho))
-    
+        return (country == u'br' and not (paid or self.show_empenho))
+    #
     @property
     def show_paypal(self):
-        ''' show only if registration not paid 
+        ''' show only if registration not paid
         '''
         paid = self.paid
         return not (paid or self.show_empenho)
-    
+    #
     def _price(self):
         view = self.price_view
         attendees = self.attendees()
-        
         qty = len(attendees)
         registration_type = self.registration_type
         discount_code = self.context.discount_code
-        price = view.price(registration_type,qty, discount_code)
+        price = view.price(registration_type, qty, discount_code)
         fmtPrice = view.fmtPrice(price)
-        return (price,fmtPrice)
-    
+        return (price, fmtPrice)
+    #
     @property
     def base_price(self):
         # Price per attendee
@@ -202,20 +216,20 @@ class View(grok.View):
         qty = len(attendees) or 1
         price = self._price()[0]
         return price / qty
-    
-    
+    #
     @property
     def price(self):
         return self._price()[0]
-    
+    #
     @property
     def fmtBasePrice(self):
         return self.price_view.fmtPrice(self.base_price)
-        
+    #
     @property
     def fmtPrice(self):
         return self._price()[1]
-
-    
-
-        
+    #
+    def available_trainings(self):
+        ''' List of available trainings '''
+        helper = self.helper
+        return helper.trainings()
