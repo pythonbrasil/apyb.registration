c34749aed0d5801d1eea48af40b09e4c7bc1f055
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-14 23:49:55 -0300

    Add methods to deal with training PEP8

--- apyb/registration/attendee.py
+++ apyb/registration/attendee.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 from Acquisition import aq_inner
+from Acquisition import aq_parent
+
 from five import grok
 from plone.directives import dexterity, form
 
@@ -8,16 +10,13 @@ from zope import schema
 from zope.schema.interfaces import IVocabularyFactory
 from zope.component import getMultiAdapter, queryUtility
 
-
 from zope.component import getUtility
 from zope.app.intid.interfaces import IIntIds
 
 from plone.indexer import indexer
 
-from z3c.form import group, field
-from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
-
 from apyb.registration import MessageFactory as _
+
 
 class IAttendee(form.Schema):
     """
@@ -28,94 +27,130 @@ class IAttendee(form.Schema):
         title=_(u"uid"),
         required=False,
         )
-    
+    #
     registration_type = schema.Choice(
         title=_(u'Type'),
         description=_(u'Select the category of your registration'),
         required=False,
-        vocabulary="apyb.registration.types"
+        vocabulary="apyb.registration.types",
     )
-    
+    #
     fullname = schema.TextLine(
         title=_(u'Fullname'),
         description=_(u'Please inform your fullname'),
         required=True,
     )
-    
+    #
     badge_name = schema.TextLine(
         title=_(u'Badge name'),
-        description=_(u'Please inform the name that will appear on your badge -- Leave it blank to use your fullname in the badge'),
+        description=_(u'Please inform the name that will appear on your badge'
+                      u' -- Leave it blank to use your fullname in the badge'),
         required=False,
         missing_value=u'',
     )
-    
+    #
     organization = schema.TextLine(
         title=_(u'Organization'),
-        description=_(u'Please inform the name of the organization you will represent'),
+        description=_(u'Please inform the name of the organization you'
+                      u'will represent'),
         required=False,
         missing_value=u'',
     )
-    
+    #
     gender = schema.Choice(
         title=_(u'Gender'),
         required=True,
-        vocabulary="apyb.registration.gender"
-        )
-
+        vocabulary="apyb.registration.gender",
+    )
+    #
     t_shirt_size = schema.Choice(
         title=_(u'T-Shirt Size'),
         required=True,
-        vocabulary="apyb.registration.tshirt"
-        )
+        vocabulary="apyb.registration.tshirt",
+    )
+    #
+    form.omitted('uid')
+    trainings = schema.List(
+        title=_(u'Trainings'),
+        description=_(u'Select trainings'),
+        default=[],
+        value_type=schema.Choice(title=_(u"Training"),
+                                 vocabulary='apyb.registration.trainings'),
+        required=False,
+    )
 
 
 class Attendee(dexterity.Item):
     grok.implements(IAttendee)
-    
+    #
     def _get_title(self):
         return self.fullname
+    #
     def _set_title(self, value):
         pass
+    #
     title = property(_get_title, _set_title)
-    
+   #
     def Title(self):
         return self.title
-    
+    #
     @property
     def uid(self):
         intids = getUtility(IIntIds)
         return intids.getId(self)
-    
+    #
     def UID(self):
         return self.uid
 
+
 @indexer(IAttendee)
 def registration_type(obj):
-    return [obj.registration_type,]
+    return [obj.registration_type, ]
 grok.global_adapter(registration_type, name="Subject")
 
 
 class View(grok.View):
     grok.context(IAttendee)
     grok.require('zope2.View')
-    
+    #
     def update(self):
-        super(View,self).update()
+        super(View, self).update()
         context = aq_inner(self.context)
+        registration = aq_parent(context)
+        registrations = aq_parent(registration)
         self.registration_type = self.context.registration_type
         self._path = '/'.join(context.getPhysicalPath())
-        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
-        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
-        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self.state = getMultiAdapter((context, self.request),
+                                      name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request),
+                                      name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request),
+                                      name=u'plone_portal_state')
+        self.helper = getMultiAdapter((registrations, self.request),
+                                      name=u'helper')
         self._ct = self.tools.catalog()
         self.member = self.portal.member()
-        self.voc = queryUtility(IVocabularyFactory, 'apyb.registration.types')(context)
-        roles_context = self.member.getRolesInContext(context)
-        if not [r for r in roles_context if r in ['Manager','Editor','Reviewer',]]:
+        voc_factory = queryUtility(IVocabularyFactory,
+                                   'apyb.registration.types')
+        self.voc = voc_factory(context)
+        self.roles_context = self.member.getRolesInContext(context)
+        if not [r for r in self.roles_context
+                        if r in ['Manager', 'Editor', 'Reviewer', ]]:
             self.request['disable_border'] = True
-    
+    #
+    @property
+    def can_register_trainings(self):
+        review_state = self.state.workflow_state()
+        return review_state == 'confirmed'
+    # 
+    def available_trainings(self):
+        ''' List of available trainings '''
+        helper = self.helper
+        return helper.trainings()
+    #
     @property
     def fmt_registration_type(self):
         registration_type = self.registration_type
         if registration_type:
-            return self.voc.getTerm(registration_type).title
+            term = self.voc.getTerm(registration_type)
+            return term.title
